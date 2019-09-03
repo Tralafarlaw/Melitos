86dc9e65c309351b6020f792837d7166521e1d9f
Tralafarlaw <tralafarlaw@gmail.com>
2019-09-02 22:36:27 -0400

    initial commit

---
+++ app/src/main/java/com/amuyu/melitos/Activitis/LogoActivity.java
@@ -0,0 +1,16 @@
+package com.amuyu.melitos.Activitis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.amuyu.melitos.R;
+
+public class LogoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logo);
+    }
+}
---
+++ app/src/main/java/com/amuyu/melitos/CONSTANTES.java
@@ -0,0 +1,6 @@
+package com.amuyu.melitos;
+
+public final class CONSTANTES {
+    //Aca ban cosas como los path para la db los nombres de los campos el valor de pi y esas cosas
+    public final String APP_NAME = "Melitos";
+}
